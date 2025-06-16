0e77a0cbf0feebfe06d7e69e54e57810494548cc
areias03 <alexandre@qut-lpa00161720.qut.edu.au>
2025-06-16 10:12:15 +1000

    caching of large SPIRE data

--- spirepy/data.py
+++ spirepy/data.py
@@ -1,6 +1,8 @@
 import polars as pl
+from functools import cache
 
 
+@cache
 def cluster_metadata():
     return pl.read_csv(
         "https://swifter.embl.de/~fullam/spire/metadata/spire_v1_cluster_metadata.tsv.gz",
@@ -8,6 +10,7 @@ def cluster_metadata():
     )
 
 
+@cache
 def genome_metadata():
     return pl.read_csv(
         "https://swifter.embl.de/~fullam/spire/metadata/spire_v1_genome_metadata.tsv.gz",
