700d6937e5f7866de3b5e5f2909375b2f3b176b0
areias03 <alexandre@qut-lpa00161720.qut.edu.au>
2025-06-27 13:21:51 +1000

    ADD caching to disk of cluster and genome metadata

--- spirepy/data.py
+++ spirepy/data.py
@@ -1,17 +1,23 @@
 import polars as pl
-from functools import cache
+from joblib import Memory
+from platformdirs import user_cache_dir
+
+cache_dir = user_cache_dir("spirepy", "spirepy-dev")
+memory = Memory(cache_dir, verbose=0)
 
 
-@cache
+@memory.cache
 def cluster_metadata():
+    """Fetches and caches the SPIRE cluster metadata from the remote server."""
     return pl.read_csv(
         "https://swifter.embl.de/~fullam/spire/metadata/spire_v1_cluster_metadata.tsv.gz",
         separator="\t",
     )
 
 
-@cache
+@memory.cache
 def genome_metadata():
+    """Fetches and caches the SPIRE genome metadata from the remote server."""
     return pl.read_csv(
         "https://swifter.embl.de/~fullam/spire/metadata/spire_v1_genome_metadata.tsv.gz",
         separator="\t",
