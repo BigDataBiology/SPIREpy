5ec51a7454aa23c5b2cef87c1c21dd00f9fef431
Luis Pedro Coelho <luis@luispedro.org>
2025-06-30 11:37:49 +1000

    MIN Use SPIREpy as logger name

--- spirepy/logger.py
+++ spirepy/logger.py
@@ -5,5 +5,5 @@ formatter = logging.Formatter("%(message)s")
 handler = RichHandler(level=logging.WARNING, markup=True)
 handler.setFormatter(formatter)
 
-logger = logging.getLogger("micro_fr_pred")
+logger = logging.getLogger("SPIREpy")
 logger.addHandler(handler)
