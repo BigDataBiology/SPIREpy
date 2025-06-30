6e4d027493d6142340ccae5d1982d0e6b0d17aff
Luis Pedro Coelho <luis@luispedro.org>
2025-06-30 11:37:40 +1000

    DOC Better docstrings
    Mention the caching & return values

--- spirepy/data.py
+++ spirepy/data.py
@@ -8,7 +8,13 @@ memory = Memory(cache_dir, verbose=0)
 
 @memory.cache
 def cluster_metadata():
-    """Fetches and caches the SPIRE cluster metadata from the remote server."""
+    """Fetches and caches the SPIRE cluster metadata from the remote server.
+
+    This is slow for the first time, as it downloads a large file, but
+    subsequent calls will use the cached version.
+
+    :return: A DataFrame with the SPIRE cluster metadata.
+    """
     return pl.read_csv(
         "https://swifter.embl.de/~fullam/spire/metadata/spire_v1_cluster_metadata.tsv.gz",
         separator="\t",
@@ -17,7 +23,13 @@ def cluster_metadata():
 
 @memory.cache
 def genome_metadata():
-    """Fetches and caches the SPIRE genome metadata from the remote server."""
+    """Fetches and caches the SPIRE genome metadata from the remote server.
+
+    This is slow for the first time, as it downloads a large file, but
+    subsequent calls will use the cached version.
+
+    :return: A DataFrame with the SPIRE genome metadata.
+    """
     return pl.read_csv(
         "https://swifter.embl.de/~fullam/spire/metadata/spire_v1_genome_metadata.tsv.gz",
         separator="\t",
